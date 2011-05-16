39cd67de9fdb451893ee5da8f8f51d1cf59d3aa9
Robert J. Macomber <robertm@dilithium>
2011-05-16 09:22:35 -0700

    Publish to website

--- project/build/Json.scala
+++ project/build/Json.scala
@@ -1,9 +1,8 @@
 import sbt._
 
-class Json(info: ProjectInfo) extends DefaultProject(info) {
-  override def compileOptions =
-    compileOptions("-encoding", "UTF-8") ++ compileOptions("-g") ++ compileOptions("-unchecked") ++ super.compileOptions
-
+class Json(info: ProjectInfo) extends DefaultProject(info) with rsync.RsyncPublishing {
   lazy val scalaTest = "org.scalatest" % "scalatest" % "1.3" % "test->default"
   lazy val scalaCheck = "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test->default"
+
+  def rsyncRepo = "rojoma.com:public_html/maven"
 }
