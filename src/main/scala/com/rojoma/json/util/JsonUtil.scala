824478e3557d4a6f18d72824fdc65a7d549c9dde
Robert J. Macomber <robertm@dilithium>
2011-05-21 06:41:48 -0700

    Utilities for common chains of actions
    Currently just charstream <--> object chains

---
+++ src/main/scala/com/rojoma/json/util/JsonUtil.scala
@@ -0,0 +1,46 @@
+package com.rojoma.json
+package util
+
+import java.io.{Reader, BufferedReader, Writer, FilterWriter, BufferedWriter}
+
+import ast._
+import io._
+import codec._
+
+object JsonUtil {
+  def readJson[T : JsonCodec](reader: Reader, buffer: Boolean = false) = {
+    val finalReader = if(buffer) new BufferedReader(reader)
+                      else reader
+    JsonCodec.fromJValue[T](JsonReader.fromReader(finalReader))
+  }
+
+  def parseJson[T : JsonCodec](string: String) = JsonCodec.fromJValue[T](JsonReader.fromString(string))
+
+  def writeJson[T : JsonCodec](writer: Writer, jsonable: T, pretty: Boolean = false, buffer: Boolean = false) = {
+    val json = JsonCodec.toJValue(jsonable)
+
+    def write(finalWriter: Writer) {
+      if(pretty) PrettyJsonWriter.toWriter(finalWriter, json)
+      else CompactJsonWriter.toWriter(finalWriter, json)
+    }
+
+    if(buffer) {
+      val barrier = new FilterWriter(writer) {
+        override def close() {}
+        override def flush() {}
+      }
+      val buffer = new BufferedWriter(barrier)
+      write(buffer)
+      buffer.flush()
+    } else {
+      write(writer)
+    }
+  }
+
+  def renderJson[T : JsonCodec](jsonable: T, pretty: Boolean = false) = {
+    val json = JsonCodec.toJValue(jsonable)
+    if(pretty) PrettyJsonWriter.toString(json)
+    else CompactJsonWriter.toString(json)
+  }
+}
+
