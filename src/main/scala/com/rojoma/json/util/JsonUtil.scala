43d16c4cb5e0b7d1a4cd7868071969955750268e
Robert J. Macomber <robertm@dilithium>
2011-05-21 09:35:04 -0700

    More @throws

--- src/main/scala/com/rojoma/json/util/JsonUtil.scala
+++ src/main/scala/com/rojoma/json/util/JsonUtil.scala
@@ -1,21 +1,25 @@
 package com.rojoma.json
 package util
 
-import java.io.{Reader, BufferedReader, Writer, FilterWriter, BufferedWriter}
+import java.io.{Reader, BufferedReader, Writer, FilterWriter, BufferedWriter, IOException}
 
 import ast._
 import io._
 import codec._
 
 object JsonUtil {
+  @throws(classOf[IOException])
+  @throws(classOf[JsonParseException])
   def readJson[T : JsonCodec](reader: Reader, buffer: Boolean = false) = {
     val finalReader = if(buffer) new BufferedReader(reader)
                       else reader
     JsonCodec.fromJValue[T](JsonReader.fromReader(finalReader))
   }
 
+  @throws(classOf[JsonParseException])
   def parseJson[T : JsonCodec](string: String) = JsonCodec.fromJValue[T](JsonReader.fromString(string))
 
+  @throws(classOf[IOException])
   def writeJson[T : JsonCodec](writer: Writer, jsonable: T, pretty: Boolean = false, buffer: Boolean = false) = {
     val json = JsonCodec.toJValue(jsonable)
 
