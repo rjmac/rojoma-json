6d3732385c9f0c69726d9a367c6edec8631047da
Robert J. Macomber <robertm@dilithium>
2011-05-23 06:56:20 -0700

    Honor JSONx's limits

--- src/main/scala/com/rojoma/json/io/JsonXWriter.scala
+++ src/main/scala/com/rojoma/json/io/JsonXWriter.scala
@@ -3,38 +3,50 @@ package io
 
 import ast._
 
+// For the random arbitrary limits on sizes JSONx specifies
 sealed abstract class JsonXException(msg: String) extends Exception(msg)
-case class InvalidRootElementException() extends JsonXException("Root element must be an object or array")
+case class StringTooLongException() extends JsonXException("Maximum string-length exceeded")
+case class MaximumNestingDepthExceeded() extends JsonXException("Maximum nesting depth exeeded")
 
 object JsonX {
   @throws(classOf[JsonXException])
-  def toXml(x: JValue): xml.Elem = x match {
-    case obj: JObject => schemaize(innerToXml(obj))
-    case arr: JArray => schemaize(innerToXml(arr))
-    case _ => throw InvalidRootElementException()
+  def toXml(x: JCompound): xml.Elem = x match {
+    case obj: JObject => schemaize(innerToXml(obj, 0))
+    case arr: JArray => schemaize(innerToXml(arr, 0))
   }
+
+  val MaximumStringLength = 8192
+  val MaximumNestingDepth = 64
+  // max length for Numbers is 128; since we use Longs & Doubles, we
+  // can't reach that.
 
   private def schemaize(e: xml.Elem): xml.Elem = {
     def att(ns: String, name: String, value: String) = new xml.PrefixedAttribute(ns, name, value, xml.Null)
     e % att("xsi", "schemaLocation", "http://www.datapower.com/schemas/json jsonx.xsd") % att("xmlns", "xsi", "http://www.w3.org/2001/XMLSchema-instance") % att("xmlns", "json", "http://www.ibm.com/xmlns/prod/2009/jsonx")
   }
 
-  private def innerToXml(x: JValue): xml.Elem = x match {
-    case JNull =>
-      <json:null/>
-    case JIntegral(x) =>
-      <json:number>{x}</json:number>
-    case JFloatingPoint(x) =>
-      <json:number>{x}</json:number>
-    case JString(x) =>
-      <json:string>{x}</json:string>
-    case JBoolean(x) =>
-      <json:boolean>{x}</json:boolean>
-    case JArray(xs) =>
-      <json:array>{xs map innerToXml}</json:array>
-    case JObject(fields) =>
-      <json:object>{fields map { case (name, value) =>
-        innerToXml(value) % new xml.UnprefixedAttribute("name", name, xml.Null)
-      }}</json:object>
+  private def innerToXml(x: JValue, depth: Int): xml.Elem = {
+    if(depth > MaximumNestingDepth) throw MaximumNestingDepthExceeded()
+    x match {
+      case JNull =>
+        <json:null/>
+      case JIntegral(x) =>
+        <json:number>{x}</json:number>
+      case JFloatingPoint(x) =>
+        if(x.isNaN || x.isInfinite)
+          throw JsonInvalidFloat(x)
+        <json:number>{x}</json:number>
+      case JString(x) =>
+        if(x.length > MaximumStringLength) throw StringTooLongException()
+        <json:string>{x}</json:string>
+      case JBoolean(x) =>
+        <json:boolean>{x}</json:boolean>
+      case JArray(xs) =>
+        <json:array>{xs map (innerToXml(_, depth+1))}</json:array>
+      case JObject(fields) =>
+        <json:object>{fields map { case (name, value) =>
+          innerToXml(value, depth+1) % new xml.UnprefixedAttribute("name", name, xml.Null)
+        }}</json:object>
+    }
   }
 }
