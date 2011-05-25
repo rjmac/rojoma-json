0dd54f770a45b64013611712c682861ae711da49
Robert J. Macomber <robertm@dilithium>
2011-05-24 23:43:48 -0700

    Loosen type bounds a bit more, and get rid of all Self <:< Foo

--- src/main/scala/com/rojoma/json/zipper/JsonZipper.scala
+++ src/main/scala/com/rojoma/json/zipper/JsonZipper.scala
@@ -6,11 +6,11 @@ import ast._
 sealed trait ZipperLike[Parent] {
   def top_? : Option[JsonZipper[_]]
 
-  def up(implicit ev: Parent <:< JCompoundZipper[_]): Parent
+  def up(implicit ev: Parent <:< JsonZipper[_]): Parent
   def up_? : Option[JsonZipper[_]]
 
-  def next(implicit ev: Parent <:< JCompoundZipper[_]): Option[JsonZipper[Parent]]
-  def prev(implicit ev: Parent <:< JCompoundZipper[_]): Option[JsonZipper[Parent]]
+  def next(implicit ev: Parent <:< JsonZipper[_]): Option[JsonZipper[Parent]]
+  def prev(implicit ev: Parent <:< JsonZipper[_]): Option[JsonZipper[Parent]]
 
   def next_? : Option[JsonZipper[Parent]]
   def prev_? : Option[JsonZipper[Parent]]
@@ -18,8 +18,8 @@ sealed trait ZipperLike[Parent] {
   def position(implicit ev: Parent <:< JArrayZipper[_]): Int
   def field(implicit ev: Parent <:< JObjectZipper[_]): String
 
-  def position_?(implicit ev: Parent <:< JArrayZipper[_]): Option[Int]
-  def field_?(implicit ev: Parent <:< JObjectZipper[_]): Option[String]
+  def position_? : Option[Int]
+  def field_? : Option[String]
 
   def replace(newValue: JAtom): JAtomZipper[Parent]
   def replace(newValue: JArray): JArrayZipper[Parent]
@@ -33,7 +33,7 @@ sealed trait ZipperLike[Parent] {
 }
 
 sealed trait NothingZipper[Parent] extends ZipperLike[Parent] {
-  def top(implicit ev: Parent <:< JCompoundZipper[_]): JsonZipper[_]
+  def top(implicit ev: Parent <:< JsonZipper[_]): JsonZipper[_]
 }
 
 sealed trait JsonZipper[Parent] extends ZipperLike[Parent] {
@@ -43,23 +43,16 @@ sealed trait JsonZipper[Parent] extends ZipperLike[Parent] {
   def top: JsonZipper[_]
   def top_? = Some(top)
 
-  def down(idx: Int)(implicit ev: Self <:< JArrayZipper[_]): JsonZipper[Self]
-  def down(field: String)(implicit ev: Self <:< JObjectZipper[_]): JsonZipper[Self]
-
   def down_?(idx: Int): Option[JsonZipper[Self]]
   def down_?(field: String): Option[JsonZipper[Self]]
-
-  def first(implicit ev: Self <:< JCompoundZipper[_]): JsonZipper[Self]
-  def last(implicit ev: Self <:< JCompoundZipper[_]): JsonZipper[Self]
 
   def first_? : Option[JsonZipper[Self]]
   def last_? : Option[JsonZipper[Self]]
 
   def remove: NothingZipper[Parent]
-  def remove(idx: Int)(implicit ev: Self <:< JArrayZipper[_]): Self = down(idx).remove.up
-  def remove(field: String)(implicit ev: Self <:< JObjectZipper[_]): Self = down(field).remove.up
 
   def asAtom = this.cast[JAtomZipper[Parent]]
+  def asCompound = this.cast[JCompoundZipper[Parent]]
   def asArray = this.cast[JArrayZipper[Parent]]
   def asObject = this.cast[JObjectZipper[Parent]]
 }
@@ -68,14 +61,8 @@ sealed trait JAtomZipper[Parent] extends JsonZipper[Parent] {
   type Self = JAtomZipper[Parent]
   def here: JAtom
 
-  def down(idx: Int)(implicit ev: Self <:< JArrayZipper[_]): JsonZipper[Self] = JsonZipper.badUserNoCookie()
-  def down(field: String)(implicit ev: Self <:< JObjectZipper[_]): JsonZipper[Self] = JsonZipper.badUserNoCookie()
-
   def down_?(idx: Int): Option[JsonZipper[Self]] = None
   def down_?(field: String): Option[JsonZipper[Self]] = None
-
-  def first(implicit ev: Self <:< JCompoundZipper[_]): JsonZipper[Self] = JsonZipper.badUserNoCookie()
-  def last(implicit ev: Self <:< JCompoundZipper[_]): JsonZipper[Self] = JsonZipper.badUserNoCookie()
 
   def first_? : Option[JsonZipper[Self]] = None
   def last_? : Option[JsonZipper[Self]] = None
@@ -88,6 +75,9 @@ object JAtomZipper {
 sealed trait JCompoundZipper[Parent] extends JsonZipper[Parent] {
   type Self <: JCompoundZipper[Parent]
   def here: JCompound
+
+  def first: JsonZipper[Self]
+  def last: JsonZipper[Self]
 }
 
 sealed trait JArrayZipper[Parent] extends JCompoundZipper[Parent] {
@@ -101,7 +91,6 @@ sealed trait JArrayZipper[Parent] extends JCompoundZipper[Parent] {
   }
 
   def down(idx: Int)(implicit ev: Self <:< JArrayZipper[_]): JsonZipper[Self] = subZipper(here(idx), idx)
-  def down(field: String)(implicit ev: Self <:< JObjectZipper[_]): JsonZipper[Self] = JsonZipper.badUserNoCookie()
 
   def down_?(idx: Int): Option[JsonZipper[Self]] = {
     if(0 <= idx && here.toSeq.lengthCompare(idx) > 0) Some(down(idx))
@@ -109,11 +98,13 @@ sealed trait JArrayZipper[Parent] extends JCompoundZipper[Parent] {
   }
   def down_?(field: String): Option[JsonZipper[Self]] = None
 
-  def first(implicit ev: Self <:< JCompoundZipper[_]): JsonZipper[Self] = down(0)
-  def last(implicit ev: Self <:< JCompoundZipper[_]): JsonZipper[Self] = down(size - 1)
+  def first: JsonZipper[Self] = down(0)
+  def last: JsonZipper[Self] = down(size - 1)
 
   def first_? : Option[JsonZipper[Self]] = if(!here.isEmpty) Some(first) else None
   def last_? : Option[JsonZipper[Self]] = if(!here.isEmpty) Some(last) else None
+
+  def remove(idx: Int): Self = down(idx).remove.up
 
   def map[P](f: JsonZipper[JArrayZipper[Parent]] => ZipperLike[JArrayZipper[Parent]]) = {
     here.toSeq.view.zipWithIndex.foldLeft(this : JArrayZipper[Parent]) { (newSelf, childIdx) =>
@@ -141,11 +132,12 @@ sealed trait JObjectZipper[Parent] extends JCompoundZipper[Parent] {
     case obj: JObject => new UnchangedObjectElementObjectZipper[JObjectZipper[Parent]](obj, this, field)
   }
 
-  def down(idx: Int)(implicit ev: Self <:< JArrayZipper[_]): JsonZipper[Self] = JsonZipper.badUserNoCookie()
   def down(field: String)(implicit ev: Self <:< JObjectZipper[_]): JsonZipper[Self] = subZipper(here(field), field)
 
   def down_?(idx: Int): Option[JsonZipper[Self]] = None
   def down_?(field: String): Option[JsonZipper[Self]] = if(here.contains(field)) Some(down(field)) else None
+
+  def remove(field: String): Self = down(field).remove.up
 
   def map(f: (String, JsonZipper[JObjectZipper[Parent]]) => ZipperLike[JObjectZipper[Parent]]) = {
     here.fields.foldLeft(this : JObjectZipper[Parent]) { (newSelf, fieldChild) =>
@@ -154,8 +146,8 @@ sealed trait JObjectZipper[Parent] extends JCompoundZipper[Parent] {
     }
   }
 
-  def first(implicit ev: Self <:< JCompoundZipper[_]): JsonZipper[Self] = down(fields.first)
-  def last(implicit ev: Self <:< JCompoundZipper[_]): JsonZipper[Self] = down(fields.last)
+  def first: JsonZipper[Self] = down(fields.first)
+  def last: JsonZipper[Self] = down(fields.last)
 
   def first_? : Option[JsonZipper[Self]] = if(!here.isEmpty) Some(first) else None
   def last_? : Option[JsonZipper[Self]] = if(!here.isEmpty) Some(last) else None
@@ -218,11 +210,11 @@ object JsonZipper {
 sealed abstract trait Root // phantom
 
 private[zipper] class TopLevelZipperLike { this: ZipperLike[Root] =>
-  def up(implicit ev: Root <:< JCompoundZipper[_]) = JsonZipper.badUserNoCookie()
+  def up(implicit ev: Root <:< JsonZipper[_]) = JsonZipper.badUserNoCookie()
   def up_? = None
 
-  def next(implicit ev: Root <:< JCompoundZipper[_]): Option[JsonZipper[Root]] = JsonZipper.badUserNoCookie()
-  def prev(implicit ev: Root <:< JCompoundZipper[_]): Option[JsonZipper[Root]] = JsonZipper.badUserNoCookie()
+  def next(implicit ev: Root <:< JsonZipper[_]): Option[JsonZipper[Root]] = JsonZipper.badUserNoCookie()
+  def prev(implicit ev: Root <:< JsonZipper[_]): Option[JsonZipper[Root]] = JsonZipper.badUserNoCookie()
 
   def next_? : Option[JsonZipper[Root]] = None
   def prev_? : Option[JsonZipper[Root]] = None
@@ -230,8 +222,8 @@ private[zipper] class TopLevelZipperLike { this: ZipperLike[Root] =>
   def position(implicit ev: Root <:< JArrayZipper[_]): Int = JsonZipper.badUserNoCookie()
   def field(implicit ev: Root <:< JObjectZipper[_]): String = JsonZipper.badUserNoCookie()
 
-  def position_?(implicit ev: Root <:< JArrayZipper[_]): Option[Int] = None
-  def field_?(implicit ev: Root <:< JObjectZipper[_]): Option[String] = None
+  def position_? : Option[Int] = None
+  def field_? : Option[String] = None
 
   def replace(newValue: JAtom) = new TopLevelAtomZipper(newValue)
   def replace(newValue: JArray) = new TopLevelArrayZipper(newValue)
@@ -239,7 +231,7 @@ private[zipper] class TopLevelZipperLike { this: ZipperLike[Root] =>
 }
 
 private[zipper] object TopLevelNothingZipper extends TopLevelZipperLike with NothingZipper[Root] {
-  def top(implicit ev: Root <:< JCompoundZipper[_]) = JsonZipper.badUserNoCookie()
+  def top(implicit ev: Root <:< JsonZipper[_]) = JsonZipper.badUserNoCookie()
   def top_? = None
 }
 
@@ -255,7 +247,7 @@ private[zipper] class TopLevelObjectZipper(val here: JObject) extends TopLevelZi
 // CODE COMMON TO ALL "ELEMENT" ZIPPER(LIKE)S
 
 private[zipper] abstract class ElementZipperLike[Parent <: JCompoundZipper[_]](val parent: Parent) { this: ZipperLike[Parent] =>
-  def up(implicit ev: Parent <:< JCompoundZipper[_]) = parent
+  def up(implicit ev: Parent <:< JsonZipper[_]) = parent
   def up_? = Some(up)
 
   def next_? = next
@@ -275,7 +267,7 @@ private[zipper] trait ElementZipper[Parent <: JCompoundZipper[_]] { this: Elemen
 }
 
 private[zipper] trait ElementNothingZipper[Parent <: JCompoundZipper[_]] { this: ElementZipperLike[Parent] with NothingZipper[Parent] =>
-  def top(implicit ev: Parent <:< JCompoundZipper[_]): JsonZipper[_] = up.top
+  def top(implicit ev: Parent <:< JsonZipper[_]): JsonZipper[_] = up.top
   def top_? = Some(up.top)
 }
 
@@ -288,25 +280,19 @@ private[zipper] abstract class ArrayElementZipperLike[Parent <: JArrayZipper[_]]
 
   def remove = new ChangedArrayElementNothingZipper(parent, idxInParent)
 
-  def next(implicit ev: Parent <:< JCompoundZipper[_]): Option[JsonZipper[Parent]] = {
-    val p = up
-    val nextIdx = idxInParent + 1
-    if(nextIdx < p.size) Some(p.down(nextIdx))
-    None
+  def next(implicit ev: Parent <:< JsonZipper[_]): Option[JsonZipper[Parent]] = {
+    up.down_?(idxInParent + 1).asInstanceOf[Option[JsonZipper[Parent]]]
   }
 
-  def prev(implicit ev: Parent <:< JCompoundZipper[_]): Option[JsonZipper[Parent]] = {
-    val p = up
-    val prevIdx = idxInParent - 1
-    if(prevIdx >= 0) Some(p.down(prevIdx))
-    None
+  def prev(implicit ev: Parent <:< JsonZipper[_]): Option[JsonZipper[Parent]] = {
+    up.down_?(idxInParent - 1).asInstanceOf[Option[JsonZipper[Parent]]]
   }
 
   def position(implicit ev: Parent <:< JArrayZipper[_]): Int = idxInParent
   def field(implicit ev: Parent <:< JObjectZipper[_]): String = JsonZipper.badUserNoCookie()
 
-  def position_?(implicit ev: Parent <:< JArrayZipper[_]): Option[Int] = Some(position)
-  def field_?(implicit ev: Parent <:< JObjectZipper[_]): Option[String] = None
+  def position_? : Option[Int] = Some(position)
+  def field_? : Option[String] = None
 }
 
 private[zipper] abstract class ArrayElementZipper[Parent <: JArrayZipper[_]](p: Parent, i: Int) extends ArrayElementZipperLike(p, i) with ElementZipper[Parent] { this: JsonZipper[Parent] =>
@@ -328,7 +314,7 @@ private[zipper] abstract class ObjectElementZipperLike[Parent <: JObjectZipper[_
 
   def remove = new ChangedObjectElementNothingZipper(parent, fieldInParent)
 
-  def next(implicit ev: Parent <:< JCompoundZipper[_]): Option[JsonZipper[Parent]] = {
+  def next(implicit ev: Parent <:< JsonZipper[_]): Option[JsonZipper[Parent]] = {
     val p = up
     p.leastAbove(fieldInParent) match {
       case Some(nextField) => Some(p.down(nextField).asInstanceOf[JsonZipper[Parent]])
@@ -336,7 +322,7 @@ private[zipper] abstract class ObjectElementZipperLike[Parent <: JObjectZipper[_
     }
   }
 
-  def prev(implicit ev: Parent <:< JCompoundZipper[_]): Option[JsonZipper[Parent]] = {
+  def prev(implicit ev: Parent <:< JsonZipper[_]): Option[JsonZipper[Parent]] = {
     val p = up
     p.greatestBelow(fieldInParent) match {
       case Some(nextField) => Some(p.down(nextField).asInstanceOf[JsonZipper[Parent]])
@@ -347,8 +333,8 @@ private[zipper] abstract class ObjectElementZipperLike[Parent <: JObjectZipper[_
   def position(implicit ev: Parent <:< JArrayZipper[_]): Int = JsonZipper.badUserNoCookie()
   def field(implicit ev: Parent <:< JObjectZipper[_]): String = fieldInParent
 
-  def position_?(implicit ev: Parent <:< JArrayZipper[_]): Option[Int] = None
-  def field_?(implicit ev: Parent <:< JObjectZipper[_]): Option[String] = Some(field)
+  def position_? : Option[Int] = None
+  def field_? : Option[String] = Some(field)
 }
 
 private[zipper] abstract class ObjectElementZipper[Parent <: JObjectZipper[_]](p: Parent, f: String) extends ObjectElementZipperLike(p, f) with ElementZipper[Parent] { this: JsonZipper[Parent] =>
@@ -381,7 +367,7 @@ private[zipper] class UnchangedObjectElementObjectZipper[Parent <: JObjectZipper
 // ARRAY ELEMENT AND CHANGED
 
 private[zipper] abstract class ChangedArrayElementZipper[Parent <: JArrayZipper[_]](p: Parent, i: Int) extends ArrayElementZipper(p, i) { this: JsonZipper[Parent] =>
-  override def up(implicit ev: Parent <:< JCompoundZipper[_]) = parent.replace(JArray(parent.here.toSeq.updated(idxInParent, here))).asInstanceOf[Parent]
+  override def up(implicit ev: Parent <:< JsonZipper[_]) = parent.replace(JArray(parent.here.toSeq.updated(idxInParent, here))).asInstanceOf[Parent]
 }
 
 private[zipper] class ChangedArrayElementAtomZipper[Parent <: JArrayZipper[_]](val here: JAtom, p: Parent, i: Int) extends ChangedArrayElementZipper(p, i) with JAtomZipper[Parent]
@@ -389,13 +375,13 @@ private[zipper] class ChangedArrayElementArrayZipper[Parent <: JArrayZipper[_]](
 private[zipper] class ChangedArrayElementObjectZipper[Parent <: JArrayZipper[_]](val here: JObject, p: Parent, i: Int) extends ChangedArrayElementZipper(p, i) with ArrayElementObjectZipper[Parent] with JObjectZipper[Parent]
 
 private[zipper] class ChangedArrayElementNothingZipper[Parent <: JArrayZipper[_]](p: Parent, i: Int) extends ArrayElementZipperLike(p, i) with ElementNothingZipper[Parent] with NothingZipper[Parent] {
-  override def up(implicit ev: Parent <:< JCompoundZipper[_]) = parent.replace(JArray(parent.here.toSeq.patch(idxInParent, Nil, 1))).asInstanceOf[Parent]
+  override def up(implicit ev: Parent <:< JsonZipper[_]) = parent.replace(JArray(parent.here.toSeq.patch(idxInParent, Nil, 1))).asInstanceOf[Parent]
 }
 
 // OBJECT ELEMENT AND CHANGED
 
 private[zipper] abstract class ChangedObjectElementZipper[Parent <: JObjectZipper[_]](p: Parent, f: String) extends ObjectElementZipper(p, f) with ElementZipper[Parent] { this: JsonZipper[Parent] =>
-  override def up(implicit ev: Parent <:< JCompoundZipper[_]) = parent.replace(JObject(parent.here.fields.updated(fieldInParent, here))).asInstanceOf[Parent]
+  override def up(implicit ev: Parent <:< JsonZipper[_]) = parent.replace(JObject(parent.here.fields.updated(fieldInParent, here))).asInstanceOf[Parent]
 }
 
 private[zipper] class ChangedObjectElementAtomZipper[Parent <: JObjectZipper[_]](val here: JAtom, p: Parent, f: String) extends ChangedObjectElementZipper(p, f) with JAtomZipper[Parent]
@@ -403,5 +389,5 @@ private[zipper] class ChangedObjectElementArrayZipper[Parent <: JObjectZipper[_]
 private[zipper] class ChangedObjectElementObjectZipper[Parent <: JObjectZipper[_]](val here: JObject, p: Parent, f: String) extends ChangedObjectElementZipper(p, f) with ObjectElementObjectZipper[Parent] with JObjectZipper[Parent]
 
 private[zipper] class ChangedObjectElementNothingZipper[Parent <: JObjectZipper[_]](parent: Parent, field: String) extends ObjectElementZipperLike(parent, field) with ElementNothingZipper[Parent] with NothingZipper[Parent] {
-  override def up(implicit ev: Parent <:< JCompoundZipper[_]) = parent.replace(JObject(parent.here.fields - field)).asInstanceOf[Parent]
+  override def up(implicit ev: Parent <:< JsonZipper[_]) = parent.replace(JObject(parent.here.fields - field)).asInstanceOf[Parent]
 }
