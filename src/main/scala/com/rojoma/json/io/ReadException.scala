a148af32ee00b6cc79cc595116249fa702e0d2f2
Robert J. Macomber <robertm@dilithium>
2011-10-28 21:58:43 -0700

    Number out of range is detected by the lexer, not the parser

--- src/main/scala/com/rojoma/json/io/ReadException.scala
+++ src/main/scala/com/rojoma/json/io/ReadException.scala
@@ -8,9 +8,9 @@ sealed abstract class JsonReaderException(message: String) extends Exception(mes
 
 sealed trait JsonLexException
 case class JsonUnexpectedCharacter(character: Char, expected: String, row: Int, col: Int) extends JsonReaderException("%d:%d: Expected %s; got character 0x%02x".format(row, col, expected, character)) with JsonLexException
+case class JsonNumberOutOfRange(number: String, row: Int, col: Int) extends JsonReaderException("Cannot store in BigDecimal: " + number) with JsonLexException
 case class JsonEOF(row: Int, col: Int) extends JsonReaderException("%d:%d: Unexpected end of input") with JsonLexException
 
 sealed trait JsonParseException
 case class JsonUnexpectedToken(token: JsonToken, expected: String, row: Int, col: Int) extends JsonReaderException("%d:%d: Expected %s; got token %s".format(row, col, expected, token.asFragment)) with JsonLexException
 case class JsonUnknownIdentifier(identifier: String, row: Int, col: Int) extends JsonReaderException("%d:%d: Unknown identifier " + identifier) with JsonParseException
-case class JsonNumberOutOfRange(number: String, row: Int, col: Int) extends JsonReaderException("Cannot store in BigDecimal: " + number) with JsonParseException
