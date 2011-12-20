fb154ea31a86afe611d8a0b5a34e22e1671a6595
Robert J. Macomber <robertm@dilithium>
2011-12-20 11:16:15 -0800

    Add "skipRestOfCompound" and "skipNextDatum" to JsonEventIterator

--- src/main/scala/com/rojoma/json/io/JsonEventIterator.scala
+++ src/main/scala/com/rojoma/json/io/JsonEventIterator.scala
@@ -13,7 +13,7 @@ case class StringEvent(string: String) extends JsonEvent
 
 case class PositionedJsonEvent(event: JsonEvent, row: Int, column: Int)
 
-class JsonEventIterator(underlying: Iterator[PositionedJsonToken]) extends Iterator[PositionedJsonEvent] {
+class JsonEventIterator(underlying: Iterator[PositionedJsonToken]) extends BufferedIterator[PositionedJsonEvent] {
   private var stack = new scala.collection.mutable.ArrayStack[State]
   stack.push(AwaitingDatum)
 
@@ -27,11 +27,60 @@ class JsonEventIterator(underlying: Iterator[PositionedJsonToken]) extends Itera
     available != null
   }
 
-  def next() = {
+  def head = {
     if(available == null && !hasNext) throw new NoSuchElementException("Empty iterator")
-    val result = available
+    available
+  }
+
+  def next() = {
+    val result = head
     available = null
     result
+  }
+
+  /**
+   * Finish reading the "current" object or list, where "current" is
+   * defined as "until the end of the most recent compound object
+   * started by `next()` or `head`, depending on whether `fromHead`
+   * is true".  If `fromHead` is false and next() has not been called,
+   * this does nothing.  If this iterator is empty, a `NoSuchElementException`
+   * will be thrown.
+   */
+  def skipRestOfCompound(fromHead: Boolean = false): JsonEventIterator = {
+    if(fromHead) {
+      next()
+    } else if(stack.size == 1 && stack(0) == AwaitingDatum) {
+      return this
+    }
+    var count = 0
+    do {
+      val ev = next().event
+      ev match {
+        case StartOfObjectEvent | StartOfArrayEvent => count += 1
+        case EndOfObjectEvent | EndOfArrayEvent => count -= 1
+        case _ => /* nothing */
+      }
+    } while(hasNext && count >= 0)
+    this
+  }
+
+  /** Skips the next datum that would be returned entirely.  If the next event
+   * is the start of a list or object, `skipRestOfCompound()` is called to
+   * pass over it. If it's a field event, the field and its associated value
+   * are skipped. If it's the end of a list or object, no position change is
+   * made and the next call to `head` or `next()` will still return the end
+   * event.  Otherwise, it's an atom and is consumed.
+   *
+   * If the iterator is empty at the start of this call, `NoSuchElementException`
+   * is raised.
+   */
+  def skipNextDatum(): JsonEventIterator = {
+    head.event match {
+      case StartOfObjectEvent | StartOfArrayEvent => skipRestOfCompound(fromHead = true)
+      case FieldEvent(_) => next(); skipNextDatum()
+      case EndOfObjectEvent | EndOfArrayEvent => this
+      case _ => next(); this
+    }
   }
 
   sealed abstract class State {
---
+++ src/test/scala/com/rojoma/json/io/JsonEventIteratorTests.scala
@@ -0,0 +1,111 @@
+package com.rojoma.json
+package io
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.MustMatchers
+
+class JsonEventIteratorTests extends FunSuite with MustMatchers {
+  def r(s: String) = new java.io.StringReader(s)
+  def i(s: String) = new JsonEventIterator(new TokenIterator(r(s)))
+  def e(s: String) = i(s).next().event
+
+  test("reading single tokens that are legitimate start-of-datum tokens") {
+    e("\"hello\"") must equal (StringEvent("hello"))
+    e("true") must equal (IdentifierEvent("true"))
+    e("1.432") must equal (NumberEvent(BigDecimal("1.432")))
+    e("[") must equal (StartOfArrayEvent)
+    e("{") must equal (StartOfObjectEvent)
+  }
+
+  test("reading non start-of-datum tokens fails") {
+    evaluating { e("]") } must produce [JsonUnexpectedToken]
+    evaluating { e("}") } must produce [JsonUnexpectedToken]
+    evaluating { e(":") } must produce [JsonUnexpectedToken]
+    evaluating { e(",") } must produce [JsonUnexpectedToken]
+  }
+
+  test("skipRestOfCompound'ing from next() before calling next() does nothing") {
+    i("[1,2,3]").skipRestOfCompound().next().event must equal (StartOfArrayEvent)
+  }
+
+  test("skipRestOfCompound'ing from head before calling next() skipRestOfCompounds first element") {
+    val it = i("[1,2,3]")
+    it.skipRestOfCompound(fromHead = true).toSeq must be ('empty)
+  }
+
+  test("skipRestOfCompound'ing after calling next() to enter skipRestOfCompounds rest of the datum") {
+    var it = i("[1,2,3]")
+    it.next()
+    it.skipRestOfCompound().toSeq must be ('empty)
+
+    it = i("{hello:'world',smiling:'gnus'}")
+    it.next()
+    it.skipRestOfCompound().toSeq must be ('empty)
+  }
+
+  test("skipRestOfCompound'ing from head after calling next() to enter skipRestOfCompounds rest of the datum, if the first item is an atom") {
+    var it = i("[1,2,3]")
+    it.next()
+    it.skipRestOfCompound(fromHead = true).toSeq must be ('empty)
+
+    it = i("{hello:'world',smiling:'gnus'}")
+    it.next()
+    it.next().event must equal (FieldEvent("hello")) // skipRestOfCompound the field event
+    it.skipRestOfCompound(fromHead = true).toSeq must be ('empty)
+  }
+
+  test("skipRestOfCompound'ing from head after calling next() to enter skipRestOfCompounds the current datum, if the first item is compound") {
+    var it = i("[['a','b','c'],2,3]")
+    it.next()
+    it.skipRestOfCompound(fromHead = true).next().event must equal (NumberEvent(BigDecimal(2)))
+
+    it = i("{hello:['a','b','c'],smiling:'gnus'}")
+    it.next()
+    it.next().event must equal (FieldEvent("hello")) // skipRestOfCompound the field event
+    it.skipRestOfCompound(fromHead = true).next().event must equal (FieldEvent("smiling"))
+  }
+
+  test("skipRestOfCompound'ing at the end raises NoSuchElementException") {
+    var it = i("5")
+    it.next()
+    evaluating { it.skipRestOfCompound() } must produce [NoSuchElementException]
+  }
+
+  test("skipNextDatum at the top level reads a whole object") {
+    i("5").skipNextDatum().toSeq must be ('empty)
+    i("[1,2,3]").skipNextDatum().toSeq must be ('empty)
+  }
+
+  test("skipNextDatum within an array skips one item") {
+    var it = i("[1,2,3]")
+    it.next()
+    it.skipNextDatum().next().event must equal (NumberEvent(BigDecimal(2)))
+  }
+
+  test("skipNextDatum at the end of an array does not move") {
+    var it = i("[1]")
+    it.next()
+    it.next()
+    it.skipNextDatum().next().event must equal (EndOfArrayEvent)
+  }
+
+  test("skipNextDatum within an object skips both field and datum") {
+    var it = i("{'hello':'world','smiling','gnus'}")
+    it.next()
+    it.skipNextDatum().next().event must equal (FieldEvent("smiling"))
+
+    it = i("{'hello':'world','smiling','gnus'}")
+    it.next()
+    it.next() // position before "world"
+    it.skipNextDatum().next().event must equal (FieldEvent("smiling"))
+  }
+
+
+  test("skipNextDatum at the end of object does not move") {
+    var it = i("{'hello':'world'}")
+    it.next()
+    it.next()
+    it.next()
+    it.skipNextDatum().next().event must equal (EndOfObjectEvent)
+  }
+}
