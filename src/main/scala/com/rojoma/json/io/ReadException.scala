e22f10e39fc110586d3814d7e9b400580246b924
Robert J. Macomber <robertm@dilithium>
2012-01-31 16:16:45 -0800

    Fix error messages

--- src/main/scala/com/rojoma/json/io/ReadException.scala
+++ src/main/scala/com/rojoma/json/io/ReadException.scala
@@ -1,6 +1,16 @@
 package com.rojoma.json
 package io
 
+import ast._
+
+private[io] object pos {
+  def apply(row: Int, col: Int, restOfMessage: String, restOfMessageArgs: Any*) = {
+    val message = restOfMessage.format(restOfMessageArgs:_*)
+    if(row == -1 && col == -1) message
+    else row + ":" + col + ": " + message
+  }
+}
+
 case class NoSuchTokenException(row: Int, col: Int) extends NoSuchElementException("Empty iterator")
 
 sealed abstract class JsonReaderException(message: String) extends Exception(message) {
@@ -9,12 +19,12 @@ sealed abstract class JsonReaderException(message: String) extends Exception(mes
 }
 
 sealed trait JsonLexException
-case class JsonUnexpectedCharacter(character: Char, expected: String, row: Int, col: Int) extends JsonReaderException("%d:%d: Expected %s; got character 0x%02x".format(row, col, expected, character)) with JsonLexException
-case class JsonNumberOutOfRange(number: String, row: Int, col: Int) extends JsonReaderException("Cannot store in BigDecimal: " + number) with JsonLexException
-case class JsonEOF(row: Int, col: Int) extends JsonReaderException("%d:%d: Unexpected end of input") with JsonLexException
+case class JsonUnexpectedCharacter(character: Char, expected: String, row: Int, col: Int) extends JsonReaderException(pos(row, col, "Expected %s; got character %s", expected, JString(character.toString))) with JsonLexException
+case class JsonNumberOutOfRange(number: String, row: Int, col: Int) extends JsonReaderException(pos(row, col, "Cannot store in BigDecimal: %s", number)) with JsonLexException
+case class JsonEOF(row: Int, col: Int) extends JsonReaderException(pos(row, col, "Unexpected end of input")) with JsonLexException
 
 sealed trait JsonParseException
-case class JsonUnexpectedToken(token: JsonToken, expected: String, row: Int, col: Int) extends JsonReaderException("%d:%d: Expected %s; got token %s".format(row, col, expected, token.asFragment)) with JsonLexException
-case class JsonUnknownIdentifier(identifier: String, row: Int, col: Int) extends JsonReaderException("%d:%d: Unknown identifier " + identifier) with JsonParseException
+case class JsonUnexpectedToken(token: JsonToken, expected: String, row: Int, col: Int) extends JsonReaderException(pos(row, col, "Expected %s; got token %s", expected, token.asFragment)) with JsonLexException
+case class JsonUnknownIdentifier(identifier: String, row: Int, col: Int) extends JsonReaderException(pos(row, col, "Unknown identifier %s", JString(identifier))) with JsonParseException
 
-case class JsonBadParse(event: JsonEvent, row: Int, col: Int) extends JsonReaderException("%d:%d: Received unexpected event %s".format(event, row, col))
+case class JsonBadParse(event: JsonEvent, row: Int, col: Int) extends JsonReaderException(pos(row, col, "Received unexpected event %s", event))
