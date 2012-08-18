a5afd7a97bcc067a2ca45314d6475117611c1a35
Robert J. Macomber <robertm@dilithium>
2012-08-18 09:22:21 -0700

    Improve Scaladoc on CodecMatcher

--- src/main/scala/com/rojoma/json/matcher/CodecMatcher.scala
+++ src/main/scala/com/rojoma/json/matcher/CodecMatcher.scala
@@ -4,13 +4,15 @@ package matcher
 import ast._
 import codec._
 
-/** Pattern-matching shim for a Codec object.
- * {{{
+/** Simple class to turn a [[com.rojoma.json.codec.JsonCodec]] into something
+ * that can be used for pattern-matching.
+ *
+ * @example {{{
  * val ListOfFoo = CodecMatcher[List[Foo]]
  * val NamedBars = CodecMatcher[Map[String, Bar]]
  * value match {
- *   case ListOfFoo(foos) => ...
- *   case NamedBars(bars) => ...
+ *   case ListOfFoo(foos) => foos.foreach(_.fooify())
+ *   case NamedBars(bars) => bars("hello").doBarring()
  *   case _ => // didn't match
  * }
  * }}}
