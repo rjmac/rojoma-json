a12c3ab13c4cd29c9d04915609487db2d50a8e1b
Robert J. Macomber <robertm@dilithium>
2014-07-06 07:10:47 -0700

    Add JPath and JsonZipper

---
+++ src/main/scala/com/rojoma/json/v3/jpath/JPath.scala
@@ -0,0 +1,167 @@
+package com.rojoma.json.v3
+package jpath
+
+import ast._
+import zipper._
+
+import JPathImpl._
+
+/** A high-level interface for walking down a tree defined by a
+ * [[com.rojoma.json.ast.JValue]], returning a set of sub-values
+ * that match a path defined by a series of steps.
+ *
+ * @example {{{
+ * val v = JsonReader.fromString("""
+ *   {
+ *      "array": [1,2,3],
+ *      "object": {
+ *        "France" : [ "Paris", "Lyon", "Bordeaux" ],
+ *        "Germany" : [ "Berlin", "Munich", "Leipzig" ],
+ *        "Italy" : [ "Rome", "Florence", "Milan" ]
+ *      }
+ *   }
+ * """)
+ *
+ * def contains(z: JsonZipper, s: String) =
+ *   z.asArray.map(_.value.toSeq.contains(JString(s))).getOrElse(false)
+ *
+ * JPath(v).down("array").finish
+ *    // --> Stream(JArray(JNumber(1), JNumber(2), JNumber(3)))
+ * JPath(v).down("object").*.downFirst.finish
+ *    // --> Stream(JString("Paris"), JString("Berlin"), JString("Rome"))
+ * JPath(v).*.down("France").downFirst.finish
+ *    // --> Stream(JString("Paris"))
+ * JPath(v).**.downFirst.finish
+ *   // --> Stream(JNumber(1), JString("Paris"), JString("Berlin"), JString("Rome"))
+ * JPath(v).*.having(_.*.where(contains(_, "Lyon"))).finish
+ *   // --> Stream(JObject("France" -> ..., "Germany" -> ..., "Italy" -> ...))
+ * JPath(v).*.*.where(contains(_, "Lyon")).finish
+ *   // --> Stream(JArray(JString("Paris"), JString("Lyon"), JString("Bordeaux")))
+ * }}} */
+class JPath private (cursors: Stream[JsonZipper]) {
+  def this(input: JValue) = this(Stream(JsonZipper(input)))
+
+  /** Produce a `Stream` of [[com.rojoma.json.ast.JValue]]s, one
+   * for each current point. */
+  def finish: Stream[JValue] = cursors.map(_.value)
+
+  private def step(op: Stage): JPath = {
+    if(cursors.isEmpty) this
+    else new JPath(cursors.flatMap(op))
+  }
+
+  /** Go down into the named field.  If a current point is
+   * not a [[com.rojoma.json.ast.JObject]], or does not contain the field,
+   * it is dropped.*/
+  def down(target: String) = step(downOp(target))
+
+  /** Go down into the given index.  If a current point is
+   * not a [[com.rojoma.json.ast.JArray]], or does not contain the index,
+   * it is dropped.*/
+  def down(target: Int) = step(downOp(target))
+
+  /** Go down into all children, whether fields or array elements.  If
+   * a current point is not a [[com.rojoma.json.ast.JObject]] or
+   * [[com.rojoma.json.ast.JArray]], it is dropped. */
+  def * = step(downAllOp)
+
+  /** Go down into the last child of an array.  If a current point is not
+   * a [[com.rojoma.json.ast.JArray]], it is dropped. */
+  def downLast = step(downLastOp)
+
+  /** Go down into the first child of an array.  If a current point is not
+   * a [[com.rojoma.json.ast.JArray]], it is dropped. */
+  def downFirst = step(downFirstOp)
+
+  /** Add every descendant of the current points to the set of points. */
+  def rec = step(recOp)
+
+  /** Go down (as by `*`) and then add every descendant of the current
+   * points to the set of points (as by `rec`). */
+  def ** = *.rec
+
+  /** Filter the set of current points by a predicate. */
+  def where(pred: JsonZipper => Boolean) = step(whereOp(pred))
+
+  /** Go down into all children (as by `*`) and then filter by
+   * a predicate (as by `where`). */
+  def downWhere(pred: JsonZipper => Boolean) = *.where(pred)
+
+  /** Filter the current set of points by applying another
+   * JPath operation to them and keeping only the ones that
+   * ended up with a non-empty set of results.
+   *
+   * This is basically mark-and-return.  E.g.,
+   * {{{
+   * x.having(_.down("foo").*.where(isNumberGreaterThan(5)))
+   * }}}
+   * is the same as:
+   * {{{
+   * x.down("foo").*.where(isNumberGreaterThan(5)).up.up
+   * }}}
+   * but also works if one of the inner steps is "rec", where
+   * no fixed number of final `up` steps would suffice.
+   */
+  def having(pred: JPath => JPath) = where(z => pred(new JPath(Stream(z))).finish.nonEmpty)
+
+  /** Go up to the parents of the current points.  Any that were
+   * already at the top of the tree will be dropped. */
+  def up = step(upOp)
+
+  /** Move to the next sibling.  Any points that were at the end
+   * already, or which were not children of [[com.rojoma.json.ast.JArray]]s,
+   * will be dropped. */
+  def next = step(nextOp)
+
+  /** Move to the previous sibling.  Any points that were at the start
+   * already, or which were not children of [[com.rojoma.json.ast.JArray]]s,
+   * will be dropped. */
+  def prev = step(prevOp)
+}
+
+object JPath extends (JValue => JPath) {
+  def apply(v: JValue) = new JPath(v)
+}
+
+private [jpath] object JPathImpl {
+  type Stage = JsonZipper => Stream[JsonZipper]
+
+   def downOp(target: String)(input: JsonZipper): Stream[JsonZipper] = input match {
+    case obj: JObjectZipper => obj.down(target).toStream
+    case _ => Stream.empty
+  }
+
+   def downOp(target: Int)(input: JsonZipper): Stream[JsonZipper] = input match {
+    case arr: JArrayZipper => arr.down(target).toStream
+    case _ => Stream.empty
+  }
+
+   val downAllOp: Stage = _ match {
+    case _: JAtomZipper => Stream.empty
+    case arr: JArrayZipper => Stream.range(0, arr.size).map(arr.down_!)
+    case obj: JObjectZipper => (obj.value.fields.keys).toStream.map(obj.down_!)
+  }
+
+   val downLastOp: Stage = _ match {
+    case arr: JArrayZipper => arr.down(arr.size - 1).toStream
+    case _ => Stream.empty
+  }
+
+   val downFirstOp: Stage = _ match {
+    case arr: JArrayZipper => arr.down(0).toStream
+    case _ => Stream.empty
+  }
+
+   val recOp: Stage = input => input #:: downAllOp(input).flatMap(recOp)
+
+   def whereOp(pref: JsonZipper => Boolean)(input: JsonZipper): Stream[JsonZipper] = {
+    if(pref(input)) Stream(input)
+    else Stream.empty
+  }
+
+   val upOp: Stage = _.up.toStream
+
+   val nextOp: Stage = _.next.toStream
+
+   val prevOp: Stage = _.prev.toStream
+}
---
+++ src/main/scala/com/rojoma/json/v3/zipper/JsonZipper.scala
@@ -0,0 +1,425 @@
+package com.rojoma.json.v3
+package zipper
+
+import scala.language.implicitConversions
+import scala.annotation.tailrec
+
+import ast._
+
+/** A common parent representing both [[com.rojoma.json.zipper.JsonZipper]]s and the
+ * [[com.rojoma.json.zipper.NothingZipper]]s which result from removing items from
+ * the tree of [[com.rojoma.json.ast.JValue]]s. */
+sealed trait ZipperLike {
+  /** Move to the parent object.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the parent object,
+   *   or `None` if this is the top-level object. */
+  def up: Option[JsonZipper]
+
+  /** Move to the parent object.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the parent object.
+   * @throws NoSuchElementException if this is the top-level object. */
+  def up_! : JsonZipper
+
+  /** Move to the next element in the parent array.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the next element,
+   *   or `None` if there is no next element or if the parent is not a
+   *   [[com.rojoma.json.ast.JArray]]. */
+  def next: Option[JsonZipper]
+
+  /** Move to the previous element in the parent array.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the previous element,
+   *   or `None` if there is no previous element or if the parent is not a
+   *   [[com.rojoma.json.ast.JArray]]. */
+  def prev: Option[JsonZipper]
+
+  /** Move to the next element in the parent array.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the next element.
+   * @throws NoSuchElementException if there is no next element or if the parent is not
+   *   a [[com.rojoma.json.ast.JArray]]. */
+  def next_! = next.getOrElse(throw new NoSuchElementException("No next sibling"))
+
+  /** Move to the previous element in the parent array.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the previous element.
+   * @throws NoSuchElementException if there is no previous element or if the parent is not
+   *   a [[com.rojoma.json.ast.JArray]]. */
+  def prev_! = prev.getOrElse(throw new NoSuchElementException("No previous sibling"))
+
+  /** Move to a different field in the parent object.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the new field,
+   *   or `None` if that field does not exist or the parent is not a
+   *   [[com.rojoma.json.ast.JObject]]. */
+  def sibling(field: String): Option[JsonZipper]
+
+  /** Move to a different field in the parent object.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the new field.
+   * @throws NoSuchElementException if there is no such field or if the parent is not a
+   *   [[com.rojoma.json.ast.JObject]]. */
+  def sibling_!(field: String) = sibling(field).getOrElse(throw new NoSuchElementException("No sibling " + field))
+
+  /** Replace the current value with an atom.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the same location but
+   *   with the current value replaced. */
+  def replace(newValue: JAtom): JAtomZipper
+
+  /** Replace the current value with an array.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the same location but
+   *   with the current value replaced. */
+  def replace(newValue: JArray): JArrayZipper
+
+  /** Replace the current value with an object.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the same location but
+   *   with the current value replaced. */
+  def replace(newValue: JObject): JObjectZipper
+
+  /** Replace the current value with a new value.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the same location but
+   *   with the current value replaced. */
+  def replace(newValue: JValue): JsonZipper = newValue match {
+    case atom: JAtom => replace(atom)
+    case array: JArray => replace(array)
+    case obj: JObject => replace(obj)
+  }
+}
+
+/** A zipper referencing the hole left after a `remove` operation. */
+sealed trait NothingZipper extends ZipperLike {
+  /** Move up the chain of parents to the top of the object.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the top object,
+   *   or `None` if the top object was removed. */
+  def top : Option[JsonZipper]
+
+  /** Move up the chain of parents to the top of the object.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the top object.
+   * @throws NoSuchElementException if the top object was removed. */
+  def top_! : JsonZipper
+}
+
+/** A zipper that points somewhere in the tree defined by a [[com.rojoma.json.ast.JValue]].
+ * It can be used to move around or update the tree in a purely functional manner.
+ *
+ * @see [[com.rojoma.json.jpath.JPath]] for a higher-level read-only interface to this
+ *   functionality. */
+sealed trait JsonZipper extends ZipperLike {
+  type ValueType <: JValue
+
+  /** The value stored at this location */
+  def value: ValueType
+
+  /** Move up the chain of parents to the top of the object.
+   *
+   * @return A [[com.rojoma.json.zipper.JsonZipper]] pointing at the top object. */
+  def top : JsonZipper
+
+  /** Remove the current value from the tree.
+   *
+   * @return A [[com.rojoma.json.zipper.NothingZipper]] pointing at the hole
+   *   left by removing the current value. */
+  def remove: NothingZipper
+
+  /** Safe downcast to [[com.rojoma.json.zipper.JAtomZipper]] */
+  def asAtom = this.cast[JAtomZipper]
+  /** Safe downcast to [[com.rojoma.json.zipper.JArrayZipper]] */
+  def asArray = this.cast[JArrayZipper]
+  /** Safe downcast to [[com.rojoma.json.zipper.JObjectZipper]] */
+  def asObject = this.cast[JObjectZipper]
+}
+
+/** A [[com.rojoma.json.zipper.JsonZipper]] that points to a [[com.rojoma.json.ast.JAtom]]. */
+sealed trait JAtomZipper extends JsonZipper {
+  type ValueType = JAtom
+}
+
+object JAtomZipper {
+  def apply(value: JAtom) = new TopLevelAtomZipper(value)
+  def unapply(zipper: JsonZipper) = zipper.cast[JAtomZipper].map(_.value)
+}
+
+/** A [[com.rojoma.json.zipper.JsonZipper]] that points to a [[com.rojoma.json.ast.JArray]]. */
+sealed trait JArrayZipper extends JsonZipper {
+  type ValueType = JArray
+
+  private def subZipper(elem: JValue, idx: Int): JsonZipper = elem match {
+    case atom: JAtom => new UnchangedArrayElementAtomZipper(atom, this, idx)
+    case array: JArray => new UnchangedArrayElementArrayZipper(array, this, idx)
+    case obj: JObject => new UnchangedArrayElementObjectZipper(obj, this, idx)
+  }
+
+  def down(idx: Int): Option[JsonZipper] =
+    if(idx < 0 || idx >= length) None
+    else Some(subZipper(value(idx), idx))
+
+  def down_!(idx: Int): JsonZipper =
+    subZipper(value(idx), idx)
+
+  def first = down(0)
+  def first_! = down_!(0)
+  def last = down(size - 1)
+  def last_! = down_!(size - 1)
+
+  def remove(idx: Int) = down(idx) match {
+    case Some(child) => child.remove.up_!.asInstanceOf[JArrayZipper]
+    case None => None
+  }
+
+  def map(f: JValue => JValue): JArrayZipper
+  def collect(f: PartialFunction[JValue, JValue]): JArrayZipper
+
+  def set(idx: Int, value: JValue): JArrayZipper = down_!(idx).replace(value).up_!.asInstanceOf[JArrayZipper]
+  def :+(value: JValue): JArrayZipper
+  def +:(value: JValue): JArrayZipper
+
+  def isEmpty = value.isEmpty
+  def nonEmpty = value.nonEmpty
+
+  lazy val size = value.size
+  def length = size
+}
+
+object JArrayZipper {
+  def apply(value: JArray): JArrayZipper = new TopLevelArrayZipper(value)
+  def unapply(zipper: JsonZipper) = zipper.cast[JArrayZipper].map(_.value)
+}
+
+/** A [[com.rojoma.json.zipper.JsonZipper]] that points to a [[com.rojoma.json.ast.JObject]]. */
+sealed trait JObjectZipper extends JsonZipper {
+  type ValueType = JObject
+
+  private def subZipper(elem: JValue, field: String): JsonZipper = elem match {
+    case atom: JAtom => new UnchangedObjectElementAtomZipper(atom, this, field)
+    case array: JArray => new UnchangedObjectElementArrayZipper(array, this, field)
+    case obj: JObject => new UnchangedObjectElementObjectZipper(obj, this, field)
+  }
+
+  def down(field: String): Option[JsonZipper] = value.get(field) match {
+    case Some(elem) => Some(subZipper(elem, field))
+    case None => None
+  }
+
+  def down_!(field: String): JsonZipper = subZipper(value(field), field)
+
+  def remove(field: String): JObjectZipper =
+    down(field) match {
+      case Some(child) => child.remove.up_!.asInstanceOf[JObjectZipper]
+      case None => this
+    }
+
+  def set(field: String, value: JValue): JObjectZipper
+
+  def map(f: ((String, JValue)) => JValue): JObjectZipper
+
+  def contains(f: String) = value.contains(f)
+}
+
+object JObjectZipper {
+  def apply(value: JObject): JObjectZipper = new TopLevelObjectZipper(value)
+  def unapply(zipper: JsonZipper) = zipper.cast[JObjectZipper].map(_.value)
+}
+
+object JsonZipper {
+  def apply(value: JAtom) = JAtomZipper(value)
+  def apply(value: JArray) = JArrayZipper(value)
+  def apply(value: JObject) = JObjectZipper(value)
+  def apply(value: JValue): JsonZipper = value match {
+    case atom: JAtom => apply(atom)
+    case array: JArray => apply(array)
+    case obj: JObject => apply(obj)
+  }
+
+  def unapply(zipper: JsonZipper): Option[JValue] = Some(zipper.value)
+
+  implicit def toCastable[T <: JsonZipper](x: T): com.rojoma.`json-impl`.DownCaster[T] = new com.rojoma.`json-impl`.DownCaster(x)
+}
+
+// TOP LEVEL
+
+private[zipper] class TopLevelZipperLike { this: ZipperLike =>
+  def up = None
+  def up_! = throw new NoSuchElementException("Cannot go up from the top level")
+
+  def next = None
+  def prev = None
+
+  def sibling(field: String) = None
+
+  def replace(newValue: JAtom) = JAtomZipper(newValue)
+  def replace(newValue: JArray) = JArrayZipper(newValue)
+  def replace(newValue: JObject) = JObjectZipper(newValue)
+}
+
+private[zipper] object TopLevelNothingZipper extends TopLevelZipperLike with NothingZipper {
+  def top = None
+  def top_! = throw new NoSuchElementException("No top-level object")
+}
+
+private[zipper] class TopLevelZipper extends TopLevelZipperLike { this: JsonZipper =>
+  def remove = TopLevelNothingZipper
+  def top : JsonZipper = this
+}
+
+private[zipper] class TopLevelAtomZipper(val value: JAtom) extends TopLevelZipper with JAtomZipper
+private[zipper] class TopLevelArrayZipper(val value: JArray) extends TopLevelZipper with JArrayZipper {
+  def :+(newValue: JValue): JArrayZipper = new TopLevelArrayZipper(JArray(value.toSeq :+ newValue))
+  def +:(newValue: JValue): JArrayZipper = new TopLevelArrayZipper(JArray(newValue +: value.toSeq))
+
+  def map(f: JValue => JValue) = new TopLevelArrayZipper(JArray(value.toSeq.map(f)))
+  def collect(f: PartialFunction[JValue, JValue]) = new TopLevelArrayZipper(JArray(value.toSeq.collect(f)))
+}
+private[zipper] class TopLevelObjectZipper(val value: JObject) extends TopLevelZipper with JObjectZipper {
+  def set(newField: String, newValue: JValue): JObjectZipper = new TopLevelObjectZipper(JObject(value.fields + (newField -> newValue)))
+  def map(f: ((String, JValue)) => JValue) = new TopLevelObjectZipper(JObject(value.fields.map { kv => (kv._1, f(kv)) }))
+}
+
+// CODE COMMON TO ALL "ELEMENT" ZIPPER(LIKE)S
+
+private[zipper] abstract class ElementZipperLike[Parent <: JsonZipper](parent : Parent) { this: ZipperLike =>
+  def up_! = parent
+  def up = Some(up_!)
+}
+
+private[zipper] trait ElementZipper[Parent <: JsonZipper] { this: ElementZipperLike[Parent] with JsonZipper =>
+  def top = {
+    @tailrec
+    def loop(last: JsonZipper, next: Option[JsonZipper]): JsonZipper = next match {
+      case None => last
+      case Some(p) => loop(p, p.up)
+    }
+    loop(this, up)
+  }
+}
+
+private[zipper] trait ElementNothingZipper[Parent <: JsonZipper] { this: ElementZipperLike[Parent] with NothingZipper =>
+  def top_! : JsonZipper = up_!.top
+  def top = Some(top_!)
+}
+
+// ARRAY ELEMENT, CHANGED OR NOT
+
+private[zipper] abstract class ArrayElementZipperLike(p: JArrayZipper, val idxInParent: Int) extends ElementZipperLike(p) { this: ZipperLike =>
+  def replace(newValue: JAtom) = new ChangedArrayElementAtomZipper(newValue, up_!, idxInParent)
+  def replace(newValue: JArray) = new ChangedArrayElementArrayZipper(newValue, up_!, idxInParent)
+  def replace(newValue: JObject) = new ChangedArrayElementObjectZipper(newValue, up_!, idxInParent)
+
+  def remove = new ChangedArrayElementNothingZipper(up_!, idxInParent)
+
+  def next : Option[JsonZipper] = up_!.down(idxInParent + 1)
+  def prev : Option[JsonZipper] = up_!.down(idxInParent - 1)
+
+  def sibling(field: String) = None
+}
+
+private[zipper] abstract class ArrayElementZipper(p: JArrayZipper, i: Int) extends ArrayElementZipperLike(p, i) with ElementZipper[JArrayZipper] { this: JsonZipper =>
+}
+
+private[zipper] trait ArrayElementArrayZipper { this: ArrayElementZipper with JArrayZipper =>
+  def :+(newValue: JValue): JArrayZipper = new ChangedArrayElementArrayZipper(JArray(value.toSeq :+ newValue), up_!, idxInParent)
+  def +:(newValue: JValue): JArrayZipper = new ChangedArrayElementArrayZipper(JArray(newValue +: value.toSeq), up_!, idxInParent)
+
+  def map(f: JValue => JValue) = new ChangedArrayElementArrayZipper(JArray(value.toSeq.map(f)), up_!, idxInParent)
+  def collect(f: PartialFunction[JValue, JValue]) = new ChangedArrayElementArrayZipper(JArray(value.toSeq.collect(f)), up_!, idxInParent)
+}
+
+private[zipper] trait ArrayElementObjectZipper { this: ArrayElementZipper with JObjectZipper =>
+  def set(newField: String, newValue: JValue): JObjectZipper = new ChangedArrayElementObjectZipper(JObject(value.fields + (newField -> newValue)), up_!, idxInParent)
+
+  def map(f: ((String, JValue)) => JValue) = new ChangedArrayElementObjectZipper(JObject(value.fields.map { kv => (kv._1, f(kv)) }), up_!, idxInParent)
+}
+
+// OBJECT ELEMENT, CHANGED OR NOT
+
+private[zipper] abstract class ObjectElementZipperLike(p: JObjectZipper, val fieldInParent: String) extends ElementZipperLike(p) { this: ZipperLike =>
+  def replace(newValue: JAtom) = new ChangedObjectElementAtomZipper(newValue, up_!, fieldInParent)
+  def replace(newValue: JArray) = new ChangedObjectElementArrayZipper(newValue, up_!, fieldInParent)
+  def replace(newValue: JObject) = new ChangedObjectElementObjectZipper(newValue, up_!, fieldInParent)
+
+  def remove = new ChangedObjectElementNothingZipper(up_!, fieldInParent)
+
+  def next = None
+  def prev = None
+
+  def sibling(field: String): Option[JsonZipper] = up_!.down(field)
+}
+
+private[zipper] abstract class ObjectElementZipper(p: JObjectZipper, f: String) extends ObjectElementZipperLike(p, f) with ElementZipper[JObjectZipper] { this: JsonZipper =>
+}
+
+private[zipper] trait ObjectElementArrayZipper { this: ObjectElementZipper with JArrayZipper =>
+  def :+(newValue: JValue): JArrayZipper = new ChangedObjectElementArrayZipper(JArray(value.toSeq :+ newValue), up_!, fieldInParent)
+  def +:(newValue: JValue): JArrayZipper = new ChangedObjectElementArrayZipper(JArray(newValue +: value.toSeq), up_!, fieldInParent)
+
+  def map(f: JValue => JValue) = new ChangedObjectElementArrayZipper(JArray(value.toSeq.map(f)), up_!, fieldInParent)
+  def collect(f: PartialFunction[JValue, JValue]) = new ChangedObjectElementArrayZipper(JArray(value.toSeq.collect(f)), up_!, fieldInParent)
+}
+
+private[zipper] trait ObjectElementObjectZipper { this: ObjectElementZipper with JObjectZipper =>
+  def set(newField: String, newValue: JValue): JObjectZipper = new ChangedObjectElementObjectZipper(JObject(value.fields + (newField -> newValue)), up_!, fieldInParent)
+
+  def map(f: ((String, JValue)) => JValue) = new ChangedObjectElementObjectZipper(JObject(value.fields.map { kv => (kv._1, f(kv)) }), up_!, fieldInParent)
+}
+
+// ARRAY ELEMENT BUT UNCHANGED
+
+private[zipper] abstract class UnchangedArrayElementZipper(p: JArrayZipper, i: Int) extends ArrayElementZipper(p, i) { this: JsonZipper =>
+}
+
+private[zipper] class UnchangedArrayElementAtomZipper(val value: JAtom, p: JArrayZipper, i: Int) extends UnchangedArrayElementZipper(p, i) with JAtomZipper
+private[zipper] class UnchangedArrayElementArrayZipper(val value: JArray, p: JArrayZipper, i: Int) extends UnchangedArrayElementZipper(p, i) with ArrayElementArrayZipper with JArrayZipper
+private[zipper] class UnchangedArrayElementObjectZipper(val value: JObject, p: JArrayZipper, i: Int) extends UnchangedArrayElementZipper(p, i) with ArrayElementObjectZipper with JObjectZipper
+
+// OBJECT ELEMENT BUT UNCHANGED
+
+private[zipper] abstract class UnchangedObjectElementZipper(p: JObjectZipper, f: String) extends ObjectElementZipper(p, f) { this: JsonZipper =>
+}
+
+private[zipper] class UnchangedObjectElementAtomZipper(val value: JAtom, p: JObjectZipper, f: String) extends UnchangedObjectElementZipper(p, f) with JAtomZipper
+private[zipper] class UnchangedObjectElementArrayZipper(val value: JArray, p: JObjectZipper, f: String) extends UnchangedObjectElementZipper(p, f) with ObjectElementArrayZipper with JArrayZipper
+private[zipper] class UnchangedObjectElementObjectZipper(val value: JObject, p: JObjectZipper, f: String) extends UnchangedObjectElementZipper(p, f) with ObjectElementObjectZipper with JObjectZipper
+
+// ARRAY ELEMENT AND CHANGED
+
+private[zipper] abstract class ChangedArrayElementZipper(p: JArrayZipper, i: Int) extends ArrayElementZipper(p, i) { this: JsonZipper =>
+  override def up_! = super.up_!.replace(JArray(super.up_!.value.toSeq.updated(idxInParent, value)))
+}
+
+private[zipper] class ChangedArrayElementAtomZipper(val value: JAtom, p: JArrayZipper, i: Int) extends ChangedArrayElementZipper(p, i) with JAtomZipper
+private[zipper] class ChangedArrayElementArrayZipper(val value: JArray, p: JArrayZipper, i: Int) extends ChangedArrayElementZipper(p, i) with ArrayElementArrayZipper with JArrayZipper
+private[zipper] class ChangedArrayElementObjectZipper(val value: JObject, p: JArrayZipper, i: Int) extends ChangedArrayElementZipper(p, i) with ArrayElementObjectZipper with JObjectZipper
+
+private[zipper] class ChangedArrayElementNothingZipper(parent: JArrayZipper, i: Int) extends ArrayElementZipperLike(parent, i) with ElementNothingZipper[JArrayZipper] with NothingZipper {
+  override def up_! = super.up_!.replace(JArray(super.up_!.value.toSeq.patch(idxInParent, Nil, 1)))
+
+  override def replace(newValue: JAtom) = new ChangedArrayElementAtomZipper(newValue, parent, idxInParent)
+  override def replace(newValue: JArray) = new ChangedArrayElementArrayZipper(newValue, parent, idxInParent)
+  override def replace(newValue: JObject) = new ChangedArrayElementObjectZipper(newValue, parent, idxInParent)
+
+  override def remove = this
+
+  override def next : Option[JsonZipper] = up_!.down(idxInParent)
+}
+
+// OBJECT ELEMENT AND CHANGED
+
+private[zipper] abstract class ChangedObjectElementZipper(p: JObjectZipper, f: String) extends ObjectElementZipper(p, f) with ElementZipper[JObjectZipper] { this: JsonZipper =>
+  override def up_! = super.up_!.replace(JObject(super.up_!.value.fields.updated(fieldInParent, value)))
+}
+
+private[zipper] class ChangedObjectElementAtomZipper(val value: JAtom, p: JObjectZipper, f: String) extends ChangedObjectElementZipper(p, f) with JAtomZipper
+private[zipper] class ChangedObjectElementArrayZipper(val value: JArray, p: JObjectZipper, f: String) extends ChangedObjectElementZipper(p, f) with ObjectElementArrayZipper with JArrayZipper
+private[zipper] class ChangedObjectElementObjectZipper(val value: JObject, p: JObjectZipper, f: String) extends ChangedObjectElementZipper(p, f) with ObjectElementObjectZipper with JObjectZipper
+
+private[zipper] class ChangedObjectElementNothingZipper(parent: JObjectZipper, field: String) extends ObjectElementZipperLike(parent, field) with ElementNothingZipper[JObjectZipper] with NothingZipper {
+  override def up_! = super.up_!.replace(JObject(super.up_!.value.fields - field))
+}
