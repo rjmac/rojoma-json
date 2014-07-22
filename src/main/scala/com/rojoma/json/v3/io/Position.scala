d6470fd2cc16ab4f680627edbb7f9f5314c6b6ca
Robert J. Macomber <robertm@dilithium>
2014-07-21 17:51:10 -0700

    Position can be rendered to JSON

--- src/main/scala/com/rojoma/json/v3/io/Position.scala
+++ src/main/scala/com/rojoma/json/v3/io/Position.scala
@@ -1,6 +1,10 @@
 package com.rojoma.json.v3
 package io
 
+import ast.{JValue,JNull}
+import codec._
+import matcher._
+
 class Position(val `private once 2.10 is no more`: Long) extends AnyVal {
   def row = (`private once 2.10 is no more` >> 32).toInt
   def column = `private once 2.10 is no more`.toInt
@@ -17,4 +21,20 @@ object Position {
   def unapply(p: Position) = Some((p.row, p.column))
 
   val Invalid = Position(-1, -1)
+
+  implicit val jCodec: JsonEncode[Position] with JsonDecode[Position] = new JsonEncode[Position] with JsonDecode[Position] {
+    private val pairEnc = JsonEncode[(Int, Int)]
+    private val pairDec = JsonDecode[Either[JNull, (Int, Int)]]
+
+    def encode(p: Position) =
+      if(p == Invalid) JNull
+      else pairEnc.encode((p.row, p.column))
+    def decode(x: JValue) =
+      pairDec.decode(x).right.map {
+        case Right((r,c)) =>
+          Position(r, c)
+        case Left(JNull) =>
+          Invalid
+      }
+  }
 }
