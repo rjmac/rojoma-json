6a8da469cc350796220706c5c90166496608456c
Robert J. Macomber <robertm@dilithium>
2019-06-28 15:48:30 -0700

    Remove v2 conversions

--- src/main/scala/com/rojoma/json/v3/conversions/package.scala
+++
@@ -1,4 +0,0 @@
-package com.rojoma.json.v3
-
-package object conversions extends `-impl`.conversions.v2.Conversions
-
--- src/main/scala/com/rojoma/json/v3/conversions/v2/package.scala
+++
@@ -1,8 +0,0 @@
-package com.rojoma.json.v3
-package conversions
-
-/** This is deprecated; prefer to use just `com.rojoma.json.v3.conversions`.
-  * It has no deprecated annotation because it doesn't seem to be possible
-  * to put one on a package object.
-  */
-package object v2 extends `-impl`.conversions.v2.Conversions
